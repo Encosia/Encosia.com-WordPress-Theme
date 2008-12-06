363e9f27dd69b04a67d6f45932346e993ad08736
DWard <DWard@8e589f58-77db-384f-abea-3c4620f7eb80>
2008-12-06 23:40:47 +0000

    Changed h3 to h2.
    git-svn-id: https://encosia.svn.beanstalkapp.com/encosia-wordpress-theme/trunk@13 8e589f58-77db-384f-abea-3c4620f7eb80

--- search.php
+++ search.php
@@ -8,7 +8,7 @@
 	<?php while (have_posts()) : the_post(); ?>
 	<div class="post-content" id="post-<?php the_ID(); ?>">
 	
-		  <h3><a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title(); ?>"><?php the_title(); ?></a></h3>
+		  <h2><a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title(); ?>"><?php the_title(); ?></a></h2>
 		  <?php the_excerpt(); ?>
 	</div>
 	<?php endwhile; ?>
