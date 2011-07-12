6b2f93235dde26292f48f0d5f47b24c757fabb3a
DWard <DWard@8e589f58-77db-384f-abea-3c4620f7eb80>
2011-07-12 15:28:25 +0000

    Added search back and rejiggered some verbiage and link location.
    git-svn-id: https://encosia.svn.beanstalkapp.com/encosia-wordpress-theme/trunk@19 8e589f58-77db-384f-abea-3c4620f7eb80

--- sidebar.php
+++ sidebar.php
@@ -1,31 +1,40 @@
   <div id="sidebar">
+  	<div id="SearchBlock">
+      <form method="get" id="searchform" action="http://encosia.com/">
+        <input type="text" class="search" value="" name="s" id="s" />
+        <input type="submit" id="searchsubmit" value="Search" />
+      </form>
+    </div>
+
     <?php if(!is_page(17)) { ?>
     <div id="About" class="sidebarBox">
       <a href="/about-dave-ward/"><img src="/blog/images/me-sidebar.jpg" alt="Dave Ward" width="150" height="173" align="right" /></a>
 
       <p><strong>Welcome</strong>! I hope you're in the process of finding something useful here.</p>
 
-      <p>You can keep up with my latest updates by clicking below to follow me on Twitter and/or subscribing via RSS or Email.</p>      
+      <p>You can keep up with my latest updates by <a href="http://twitter.com/Encosia" target="_blank" rel="nofollow">following me on Twitter</a>
+        and/or subscribing via <a href="http://feeds.encosia.com/Encosia" rel="nofollow">RSS</a> or
+        <a href="http://www.feedburner.com/fb/a/emailverifySubmit?feedId=654672&amp;loc=en_US" rel="nofollow">Email</a>.</p>
     </div>
     <?php } ?>
-    
-    <div id="RSS" class="sidebarBox">
-      <div>
-          <a href="http://www.feedburner.com/fb/a/emailverifySubmit?feedId=654672&amp;loc=en_US" rel="nofollow"><img src="http://i.encosia.com/blog/images/email-icon-trans.png" alt="Subscribe via email" width="38" height="38" align="middle" /></a>
-          <a href="http://www.feedburner.com/fb/a/emailverifySubmit?feedId=654672&amp;loc=en_US" rel="nofollow" class="connectLink">Get free email updates</a><br />
-      </div>
-      
-      <div>
-        <a href="http://feeds.encosia.com/Encosia" rel="nofollow"><img src="http://i.encosia.com/blog/images/rss-icon-trans.png" alt="Subscribe via RSS" width="38" height="38" align="middle" /></a>
-        <a href="http://feeds.encosia.com/Encosia" rel="nofollow" class="connectLink">Subscribe to the RSS feed</a><br />
-      </div>
-    </div>
 
     <div id="lqdmad">
       <div class="lqm_ad"  lqm_channel="1" lqm_publisher="199" lqm_zone="1" lqm_format="20"></div>
 
       <div id="lqdmrd"><a href="http://lakequincy.com/devmavens">Microsoft Advertising</a> loading...</div>
       <p class="dmAdText" style="text-align: right;"><a href="http://lakequincy.com/devmavens">Ads Via Dev<b>Mavens</b></a></p>
+    </div>
+    
+    <div id="RSS" class="sidebarBox">
+      <div>
+        <a href="http://feeds.encosia.com/Encosia" rel="nofollow"><img src="http://i.encosia.com/blog/images/rss-icon-trans.png" alt="Subscribe via RSS" width="38" height="38" align="middle" /></a>
+        <a href="http://feeds.encosia.com/Encosia" rel="nofollow" class="connectLink">Subscribe to the RSS feed</a><br />
+      </div>
+
+      <div>
+          <a href="http://www.feedburner.com/fb/a/emailverifySubmit?feedId=654672&amp;loc=en_US" rel="nofollow"><img src="http://i.encosia.com/blog/images/email-icon-trans.png" alt="Subscribe via email" width="38" height="38" align="middle" /></a>
+          <a href="http://www.feedburner.com/fb/a/emailverifySubmit?feedId=654672&amp;loc=en_US" rel="nofollow" class="connectLink">Get email updates when I post</a><br />
+      </div>
     </div>
 
     <div id="Twitter" class="sidebarBox">
