0049d076895f50d7ed2295c3a50fda8ed14a8950
DWard <DWard@8e589f58-77db-384f-abea-3c4620f7eb80>
2012-01-06 07:47:23 +0000

    Display modified date instead of post date for posts that have been modified.
    git-svn-id: https://encosia.svn.beanstalkapp.com/encosia-wordpress-theme/trunk@31 8e589f58-77db-384f-abea-3c4620f7eb80

--- index.php
+++ index.php
@@ -9,7 +9,12 @@
 	  <div class="entry">
         <h2><a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title(); ?>"><?php the_title(); ?></a></h2>
 		<span class="post-cat"><?php the_category(', ') ?></span> 
-        <span class="post-calendar">By <strong><a href="/about-dave-ward/">Dave Ward</a></strong> on <?php the_time('F jS, Y') ?></span> 
+    <span class="post-calendar">By <strong><a href="/about-dave-ward/" rel="author" title="About Dave Ward">Dave Ward</a></strong>.
+    <?php if(get_the_modified_date() == get_the_date()) { ?>
+      Posted <?php the_date() ?>
+    <?php } else { ?>
+      Updated <?php the_modified_date() ?>
+    <?php } ?></span>
 		<div class="post-content">
 			<?php the_content('Click here to read the rest of this post &raquo;'); ?>
 		</div>
