9bd1d83060c00af90f8ee03b86bc103551eb97e1
Dave <Dave@8e589f58-77db-384f-abea-3c4620f7eb80>
2013-11-10 17:11:44 +0000

    Removed Tekpub link from sidebar.
    git-svn-id: https://encosia.svn.beanstalkapp.com/encosia-wordpress-theme/trunk@38 8e589f58-77db-384f-abea-3c4620f7eb80

--- sidebar.php
+++ sidebar.php
@@ -42,8 +42,4 @@
 
       <?php GoogleAnalyticsPopularPosts_view(false); ?>
     </div>
-    
-	<div id="tekpub" class="sidebarBox">
-	  <a href="http://tekpub.com/productions/jquery?ref=encosia" target="_blank"><img src="/blog/images/mastering-jquery-slide.jpg" width="336" height="163" alt="Mastering jQuery at TekPub" /></a>
-	</div>
   </div>
