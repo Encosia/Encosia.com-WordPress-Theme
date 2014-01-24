3bab0152d537b6b0791022f1dd8ecabd288d5e00
Dave Ward <dave@encosia.com>
2014-01-24 06:05:44 -0500

    Updated script references to match new structure.

--- footer.php
+++ footer.php
@@ -6,20 +6,20 @@
     <script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
     <script>
       if (typeof jQuery === 'undefined') {
-        document.write(unescape('%3Cscript%20src%3D%22/blog/includes/jquery-1.9.1.min.js%22%3E%3C/script%3E'));
+        document.write(unescape('%3Cscript%20src%3D%22/blog/wp-content/themes/encosia/js/dist/jquery-1.9.1.min.js%22%3E%3C/script%3E'));
         var jQueryType = 'Google CDN Fallback';
       } else {
         var jQueryType = 'Google CDN';
       }
     </script>
     <?php } else { ?>
-    <script src="/blog/wp-content/themes/encosia/js/jquery-1.9.1.min.js"></script>
+    <script src="/blog/wp-content/themes/encosia/js/dist/jquery-1.9.1.min.js"></script>
     <script>var jQueryType = 'Local';</script>
     <?php } ?>
     <script>var jQueryLoadTime = new Date().getTime() - jQueryLoadStart;</script>
 
     <script src="/blog/wp-content/themes/encosia/js/dist/plugins.min.js?v=2"></script>
-    <script src="/blog/wp-content/themes/encosia/js/encosia.all.js?v=17"></script>
+    <script src="/blog/wp-content/themes/encosia/js/dist/encosia.js?v=17"></script>
     <script src="https://apis.google.com/js/plusone.js"></script>
 
     <!-- Clicky -->
