d0fdc936c621a2cbd37bef81bdeb74db174d5343
Dave Ward <dave@encosia.com>
2014-01-24 18:06:06 -0500

    Added search form click handler to disable the button while searching.

--- js/dist/encosia.js
+++ js/dist/encosia.js
@@ -1,5 +1,22 @@
 // Initialize image zooming, but limit that slow selector to links in the post area.
-$('.post').find('a[rel=attachment]').colorbox({ rel: 'nofollow' });;(function($) {
+$('.post').find('a[rel=attachment]').colorbox({ rel: 'nofollow' });
+
+// Uno clicko, por favor.
+$('#searchform').on('submit', function(evt) {
+  // If the search field is empty, bail.
+  if (document.getElementById('s').value === '')
+    return false;
+
+  // Else
+  var searchSubmit = document.getElementById('searchsubmit');
+
+  // Disable the button to prevent double clicks.
+  searchSubmit.disabled = true;
+
+  // Show a "not allowed" pointer while submitting the search.
+  searchSubmit.style.cursor = 'not-allowed';
+});
+;(function($) {
   $.getJSON('http://encosia-latest-tweet.azurewebsites.net?callback=?',
     function(response) {
       // Preferably, look for the first non-reply, non-conferenceSpam tweet.
--- js/src/encosia.ui.js
+++ js/src/encosia.ui.js
@@ -1,2 +1,18 @@
 // Initialize image zooming, but limit that slow selector to links in the post area.
 $('.post').find('a[rel=attachment]').colorbox({ rel: 'nofollow' });
+
+// Uno clicko, por favor.
+$('#searchform').on('submit', function(evt) {
+  // If the search field is empty, bail.
+  if (document.getElementById('s').value === '')
+    return false;
+
+  // Else
+  var searchSubmit = document.getElementById('searchsubmit');
+
+  // Disable the button to prevent double clicks.
+  searchSubmit.disabled = true;
+
+  // Show a "not allowed" pointer while submitting the search.
+  searchSubmit.style.cursor = 'not-allowed';
+});
