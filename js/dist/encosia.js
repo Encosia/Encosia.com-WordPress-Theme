d16e7f738dd44016fedc13e909ad40c8951c9b6c
Dave Ward <dave@encosia.com>
2014-01-24 20:21:24 -0500

    Moved analytics into encosia.analytics.js
    - Switched Clicky to async script reference so that it could load from a
    separate include.
    - Shuffled Google's around so everything is inside the IIFE.

--- js/dist/encosia.js
+++ js/dist/encosia.js
@@ -1,4 +1,45 @@
-// Initialize image zooming, but limit that slow selector to links in the post area.
+// Clicky
+(function() {
+  // Push WordPress usernames and emails into Clicky.
+  function clicky_get_cookie( name ) {
+    var ca = document.cookie.split(';');
+
+    for (var i in ca ) {
+      if (ca[i].indexOf( name+'=' ) > -1 ) {
+        return decodeURIComponent( ca[i].split('=')[1] );
+      }
+    }
+
+    // Else
+    return '';
+  }
+
+  window.clicky_custom_session = {
+    username: clicky_get_cookie('comment_author_f0a237db1cb71445787d907e54df6077'),
+    email: clicky_get_cookie('comment_author_email_f0a237db1cb71445787d907e54df6077')
+  };
+
+  window.clicky_site_ids = [34742];
+
+  var s = document.createElement('script');
+  s.type = 'text/javascript';
+  s.async = true;
+  s.src = '//static.getclicky.com/js';
+  ( document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0] ).appendChild( s );
+})();
+
+// Google Analytics
+(function() {
+  window._gaq = _gaq || [];
+  window._gaq.push(['_setAccount', 'UA-1170191-1']);
+  window._gaq.push(['_trackTiming', 'jQuery', 'Load Time', jQueryLoadTime, jQueryType, 100]);
+  window._gaq.push(['_trackPageview']);
+
+  var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+  ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+})();
+;// Initialize image zooming, but limit that slow selector to links in the post area.
 $('.post').find('a[rel=attachment]').colorbox({ rel: 'nofollow' });
 
 // Uno clicko, por favor.
@@ -15,27 +56,7 @@ $('#searchform').on('submit', function(evt) {
 
   // Show a "not allowed" pointer while submitting the search.
   searchSubmit.style.cursor = 'not-allowed';
-});
-
-// Push WordPress usernames and emails into Clicky.
-function clicky_get_cookie( name ) {
-  var ca = document.cookie.split(';');
-
-  for (var i in ca ) {
-    if (ca[i].indexOf( name+'=' ) > -1 ) {
-      return decodeURIComponent( ca[i].split('=')[1] );
-    }
-  }
-
-  // Else
-  return '';
-}
-
-window.clicky_custom_session = {
-  username: clicky_get_cookie('comment_author_f0a237db1cb71445787d907e54df6077'),
-  email: clicky_get_cookie('comment_author_email_f0a237db1cb71445787d907e54df6077')
-};
-;(function($) {
+});;(function($) {
   $.getJSON('http://encosia-latest-tweet.azurewebsites.net?callback=?',
     function(response) {
       // Preferably, look for the first non-reply, non-conferenceSpam tweet.
---
+++ js/src/encosia.analytics.js
@@ -0,0 +1,41 @@
+// Clicky
+(function() {
+  // Push WordPress usernames and emails into Clicky.
+  function clicky_get_cookie( name ) {
+    var ca = document.cookie.split(';');
+
+    for (var i in ca ) {
+      if (ca[i].indexOf( name+'=' ) > -1 ) {
+        return decodeURIComponent( ca[i].split('=')[1] );
+      }
+    }
+
+    // Else
+    return '';
+  }
+
+  window.clicky_custom_session = {
+    username: clicky_get_cookie('comment_author_f0a237db1cb71445787d907e54df6077'),
+    email: clicky_get_cookie('comment_author_email_f0a237db1cb71445787d907e54df6077')
+  };
+
+  window.clicky_site_ids = [34742];
+
+  var s = document.createElement('script');
+  s.type = 'text/javascript';
+  s.async = true;
+  s.src = '//static.getclicky.com/js';
+  ( document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0] ).appendChild( s );
+})();
+
+// Google Analytics
+(function() {
+  window._gaq = _gaq || [];
+  window._gaq.push(['_setAccount', 'UA-1170191-1']);
+  window._gaq.push(['_trackTiming', 'jQuery', 'Load Time', jQueryLoadTime, jQueryType, 100]);
+  window._gaq.push(['_trackPageview']);
+
+  var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+  ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+})();
