d9f591ae1df319e404f77200843f9b7dd6345b75
Dave Ward <dave@encosia.com>
2014-02-17 21:58:16 -0500

    JS Comments.

--- js/src/encosia/encosia.ui.ads.js
+++ js/src/encosia/encosia.ui.ads.js
@@ -13,8 +13,13 @@
 // Even developers need to eat...
 $(window).on('load', function() {
   var adNag = function() {
+    // If the HTML content of the #azcarbon placeholder div
+    //  is anything but empty, the ad has been loaded.
     var adLoaded = $('#azcarbon').html() !== '';
 
+    // If the ad didn't display, load a small ad nag snippet
+    //  and prepend it to the first visible post (so this works
+    //  on both the home page and individual post pages).
     if (!adLoaded) {
       var getAlert = $.get('/blog/wp-content/themes/encosia/carbon-alert.html');
 
