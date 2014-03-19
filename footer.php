a59c8a293c79b2993baacbbd85cb5df1c58bda7a
Dave Ward <dave@encosia.com>
2014-03-19 00:24:32 -0400

    Cache busting.

--- footer.php
+++ footer.php
@@ -18,12 +18,12 @@
     <?php } ?>
     <script>var jQueryLoadTime = new Date().getTime() - jQueryLoadStart;</script>
 
-    <script src="/blog/wp-content/themes/encosia/js/dist/plugins.min.js?v=3"></script>
+    <script src="/blog/wp-content/themes/encosia/js/dist/plugins.min.js?v=4"></script>
     <?php if (in_array($_SERVER['REMOTE_ADDR'], array('127.0.0.1', '::1'))) { ?>
-    <script src="/blog/wp-content/themes/encosia/js/dist/encosia.js?v=22"></script>
+    <script src="/blog/wp-content/themes/encosia/js/dist/encosia.js?v=23"></script>
     <script src="//localhost:35729/livereload.js"></script>
     <?php } else { ?>
-    <script src="/blog/wp-content/themes/encosia/js/dist/encosia.min.js?v=22"></script>
+    <script src="/blog/wp-content/themes/encosia/js/dist/encosia.min.js?v=23"></script>
     <?php } ?>
 
     <script src="//apis.google.com/js/plusone.js"></script>
