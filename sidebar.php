e104f676b48b6b7a3773875226463a9ba4af6a66
Dave Ward <dave@encosia.com>
2015-02-22 16:07:10 -0500

    Drops the i.encosia prefix so that the images load via SSL when the rest of the page is.

--- sidebar.php
+++ sidebar.php
@@ -20,12 +20,12 @@
     
     <div id="RSS" class="sidebarBox">
       <div class="item">
-        <a href="http://encosia.us2.list-manage.com/subscribe?u=4303bd3d6280e45823bf78608&id=64883f8353" rel="nofollow" target="_blank"><img src="http://i.encosia.com/blog/images/email-icon-trans.png" alt="Subscribe via email" width="38" height="38" align="middle" /></a>
+        <a href="http://encosia.us2.list-manage.com/subscribe?u=4303bd3d6280e45823bf78608&id=64883f8353" rel="nofollow" target="_blank"><img src="/blog/images/email-icon-trans.png" alt="Subscribe via email" width="38" height="38" align="middle" /></a>
         <a href="http://encosia.us2.list-manage.com/subscribe?u=4303bd3d6280e45823bf78608&id=64883f8353" rel="nofollow" target="_blank" class="connectLink">Get email updates when I post</a><br />
       </div>
 
       <div class="item">
-        <a href="http://feeds.encosia.com/Encosia" rel="nofollow"><img src="http://i.encosia.com/blog/images/rss-icon-trans.png" alt="Subscribe via RSS" width="38" height="38" align="middle" /></a>
+        <a href="http://feeds.encosia.com/Encosia" rel="nofollow"><img src="/blog/images/rss-icon-trans.png" alt="Subscribe via RSS" width="38" height="38" align="middle" /></a>
         <a href="http://feeds.encosia.com/Encosia" rel="nofollow" class="connectLink">Subscribe to the RSS feed</a><br />
       </div>
     </div>
