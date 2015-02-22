3c2b37f31a24597bafc31b2c979c6166cc1c6409
Dave Ward <dave@encosia.com>
2015-02-21 22:34:16 -0500

    Adds jit-grunt.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,4 +1,5 @@
 module.exports = function(grunt) {
+  require('jit-grunt')(grunt);
 
   // Project configuration.
   grunt.initConfig({
@@ -53,13 +54,6 @@ module.exports = function(grunt) {
       }
     }
   });
-
-  grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-contrib-concat');
-  grunt.loadNpmTasks('grunt-contrib-less');
-  grunt.loadNpmTasks('grunt-contrib-cssmin');
-
+  
   grunt.registerTask('default', ['jshint', 'concat', 'uglify', 'less', 'cssmin']);
 };
