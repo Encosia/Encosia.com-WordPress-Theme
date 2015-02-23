7c33dca8ba37d79d020467d9ce37184d48f94121
Dave Ward <dave@encosia.com>
2015-02-22 22:12:31 -0500

    Fixed mobile width alignnone images and bumped CSS/JS reference revs.

--- footer.php
+++ footer.php
@@ -18,9 +18,9 @@
     <?php } ?>
     <script>var jQueryLoadTime = new Date().getTime() - jQueryLoadStart;</script>
 
-    <script src="/blog/wp-content/themes/encosia/js/dist/plugins.min.js?v=4"></script>
+    <script src="/blog/wp-content/themes/encosia/js/dist/plugins.min.js?v=5"></script>
     <?php if (in_array($_SERVER['REMOTE_ADDR'], array('127.0.0.1', '::1'))) { ?>
-    <script src="/blog/wp-content/themes/encosia/js/dist/encosia.js?v=23"></script>
+    <script src="/blog/wp-content/themes/encosia/js/dist/encosia.js?v=24"></script>
     <script src="//localhost:35729/livereload.js"></script>
     <?php } else { ?>
     <script src="/blog/wp-content/themes/encosia/js/dist/encosia.min.js?v=23"></script>
--- header.php
+++ header.php
@@ -1,34 +1,33 @@
 <!DOCTYPE html>
 <html>
   <head>
-      <meta name="viewport" content="width=device-width, initial-scale=1" />
-  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-  <meta http-equiv="X-UA-Compatible" content="IE=Edge,chrome=1">
-  <title><?php wp_title(); ?></title>
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=Edge,chrome=1">
+    <title><?php wp_title(); ?></title>
   <?php if (in_array($_SERVER['REMOTE_ADDR'], array('127.0.0.1', '::1'))) { ?>
-  <link rel="stylesheet" href="/blog/wp-content/themes/encosia/css/dist/styles.css?v=72" type="text/css" media="screen" />
+    <link rel="stylesheet" href="/blog/wp-content/themes/encosia/css/dist/styles.css?v=73" type="text/css" media="screen" />
   <?php } else { ?>
-  <link rel="stylesheet" href="/blog/wp-content/themes/encosia/css/dist/styles.min.css?v=72" type="text/css" media="screen" />
+    <link rel="stylesheet" href="/blog/wp-content/themes/encosia/css/dist/styles.min.css?v=73" type="text/css" media="screen" />
   <?php } ?>
-      <link href='http://fonts.googleapis.com/css?family=Merriweather:400italic,400,700|Archivo+Narrow:400,400italic' rel='stylesheet' type='text/css'>
-  <!--[if gte IE 9]>
-  <style type="text/css">
-    #sidebar h3 { filter: none; }
-  </style>
-  <![endif]-->
-  <link rel="alternate" type="application/rss+xml" title="Posts" href="http://feeds.encosia.com/Encosia" />
-  <link rel="alternate" type="application/rss+xml" title="Comments" href="http://feeds.encosia.com/EncosiaComments" />
-  <link rel="pingback" href="http://encosia.com/blog/xmlrpc.php" />
-  <meta property="fb:admins" content="503531327" />
+    <link href='http://fonts.googleapis.com/css?family=Merriweather:400italic,400,700|Archivo+Narrow:400,400italic' rel='stylesheet' type='text/css'>
+    <!--[if gte IE 9]>
+    <style type="text/css">
+      #sidebar h3 { filter: none; }
+    </style>
+    <![endif]-->
+    <link rel="alternate" type="application/rss+xml" title="Posts" href="http://feeds.encosia.com/Encosia" />
+    <link rel="alternate" type="application/rss+xml" title="Comments" href="http://feeds.encosia.com/EncosiaComments" />
+    <link rel="pingback" href="http://encosia.com/blog/xmlrpc.php" />
+    <meta property="fb:admins" content="503531327" />
   <?php
     global $twitter_image;
 
     if (isset($twitter_image)) { ?>
-  <meta name="twitter:image" content="<?php echo $twitter_image[0]; ?>">
+    <meta name="twitter:image" content="<?php echo $twitter_image[0]; ?>">
   <?php } ?>
-
   <?php wp_head(); ?>
-</head>
+  </head>
 <body>
 <div id="page">
   <div id="header">
