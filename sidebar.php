1b6c89495d02612147409c45a386a029f8b4e06f
Dave Ward <dave@encosia.com>
2015-03-03 08:46:47 -0500

    Attempting to sidestep uBlock...

--- sidebar.php
+++ sidebar.php
@@ -13,10 +13,10 @@
     </div>
 
       <div id="social" class="sidebarBox">
-          <a class="social social-twitter hvr-grow" href="https://twitter.com/encosia" target="_blank"><i></i> Twitter</a>
-          <a class="social social-facebook hvr-grow" href="https://facebook.com/encosia" target="_blank"><i></i>Facebook</a>
+          <a class="social social-tw hvr-grow" href="https://twitter.com/encosia" target="_blank"><i></i> Twitter</a>
+          <a class="social social-fb hvr-grow" href="https://facebook.com/encosia" target="_blank"><i></i>Facebook</a>
           <a class="social social-github hvr-grow" href="https://github.com/encosia" target="_blank"><i></i> Github</a>
-          <a class="social social-gplus hvr-grow" href="https://plus.google.com/+Encosia" target="_blank"><i></i> Google+</a>
+          <a class="social social-gp hvr-grow" href="https://plus.google.com/+Encosia" target="_blank"><i></i> Google+</a>
           <a class="social social-rss hvr-grow" href="http://feeds.encosia.com/Encosia" target="_blank"><i></i> RSS</a>
           <a class="social social-email hvr-grow" href="http://encosia.us2.list-manage.com/subscribe?u=4303bd3d6280e45823bf78608&id=64883f8353" target="_blank">
               <i></i>
